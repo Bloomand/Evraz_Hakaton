0ea2351f9732cffb63b26b12c8b0e7074139f878
Bloomand <geldashal@gmail.com>
2023-12-22 17:01:07 +0300

    Applying registartion

---
+++ src/pages/AuthPage.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const AuthPage = () => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Здесь можно добавить код для обработки отправки формы, например, с помощью API
+
+    // После обработки формы можно сбросить значения полей
+    setFullName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h2>Регистрация</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="fullName">ФИО:</label>
+          <input
+            type="text"
+            id="fullName"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Пароль:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Зарегистрироваться</button>
+      </form>
+    </div>
+  );
+}
+
+export default AuthPage;
